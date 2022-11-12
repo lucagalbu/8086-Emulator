ee26f323f6481c0e52750fe361b9813875999341
lucagalbu <luca.galbusera@unibas.ch>
2022-11-12 12:59:22 +0100

    Added flags class

---
+++ flags/flags.hpp
@@ -0,0 +1,35 @@
+#ifndef FLAGS
+#define FLAGS
+
+class Flags
+{
+private:
+    uint16_t flags;
+
+public:
+    // position of the flags as binary mask
+    enum FlagMasks
+    {
+        S = 0b000000001,
+        Z = 0b000000010,
+        A = 0b000000100,
+        P = 0b000001000,
+        C = 0b000010000,
+        O = 0b000100000,
+        D = 0b001000000,
+        I = 0b010000000,
+        T = 0b100000000
+    };
+
+    // getters
+    bool isSet(FlagMasks flag) const { return (flags & flag); }
+
+    // setters
+    void set(FlagMasks flag) { flags |= flag; }
+    void unset(FlagMasks flag) { flags &= ~flag; }
+
+    // constructor
+    Flags() : flags(0b0) {}
+};
+
+#endif
