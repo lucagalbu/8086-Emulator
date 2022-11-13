c4b76602a0494c1778fd0f9df1913c4be46ff0c9
lucagalbu <luca.galbusera@unibas.ch>
2022-11-13 11:41:14 +0100

    Refactor opCodes
    Some operations require a different number of paramaters after the 1byte opCode.
    In the current implementation, this cannot be done, because we would need a
    function that takes an arbitrary number of parameters.
    
    For this reason, I refactored the code so that the opCode now takes the memory as
    input, computes the address of the current operation and reads the opCode directly
    from the memory. Based on the opCode, if extra arguments are required, those are
    also read from the memory.
    At the end, the IP register is updated based on how much memory has been read.

--- opCodes/opCodes.hpp
+++ opCodes/opCodes.hpp
@@ -12,17 +12,23 @@ class OpCodes
 private:
     Registers &registers;
     Flags &flags;
+    const uint8_t *memory;
 
     void AAA();
 
+    uint8_t getInstructionAddress() { return (registers.CS() + registers.IP()); }
+
 public:
-    OpCodes(Registers &registers, Flags &flags) : registers(registers), flags(flags) {}
-    void executeOperation(uint8_t opCode)
+    OpCodes(Registers &registers, Flags &flags, const uint8_t *memory) : registers(registers), flags(flags), memory(memory) {}
+    void executeNextOperation()
     {
+        uint8_t codeAddress = getInstructionAddress();
+        uint8_t opCode = memory[codeAddress];
         switch (opCode)
         {
         case 0x37:
             AAA();
+            registers.IP(registers.IP() + 1);
             break;
         }
     }
