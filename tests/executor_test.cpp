58ef591b8bf007d96d5755adecc032fc2067d673
lucagalbu <luca.galbusera@unibas.ch>
2022-12-04 23:31:21 +0100

    Unit tests: Switch to mocking

--- tests/executor_test.cpp
+++ tests/executor_test.cpp
@@ -1,78 +1,64 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "trompeloeil/doctest/trompeloeil.hpp"
 
 #include "../registers/registers.hpp"
 #include "../flags/flags.hpp"
 #include "../executor/executor.hpp"
 
+#include <iostream>
+
+class Flags_mocked : public Flags
+{
+public:
+    MAKE_CONST_MOCK1(isSet, bool(FlagMasks flag));
+    MAKE_MOCK1(set, void(FlagMasks flag));
+    MAKE_MOCK1(unset, void(FlagMasks flag));
+};
+
 TEST_CASE("Test AAA")
 {
     Registers registers;
-    Flags flags;
+    Flags_mocked flags_mocked;
     uint8_t memory[1];
-    Executor executor(registers, flags, memory);
+    Executor executor(registers, flags_mocked, memory);
 
     memory[0] = 0x37;
     registers.AX(0);
 
-    SUBCASE("Test with low nibble AL>9")
+    SUBCASE("Test with low nibble AL>9 and flag A unset")
     {
+        ALLOW_CALL(flags_mocked, isSet(Flags::A)).RETURN(false);
+        REQUIRE_CALL(flags_mocked, set(Flags::A));
+        REQUIRE_CALL(flags_mocked, set(Flags::C));
+
         registers.AL(0b0000'1111);
         executor.executeNextOperation();
 
-        bool are_CA_set = flags.isSet(Flags::A) && flags.isSet(Flags::C);
-        bool are_any_other_set = flags.isSet(Flags::O) ||
-                                 flags.isSet(Flags::D) ||
-                                 flags.isSet(Flags::I) ||
-                                 flags.isSet(Flags::T) ||
-                                 flags.isSet(Flags::S) ||
-                                 flags.isSet(Flags::Z) ||
-                                 flags.isSet(Flags::P);
-
         REQUIRE(registers.AX() == 0x0105);
-        REQUIRE(are_CA_set);
-        REQUIRE_FALSE(are_any_other_set);
     };
 
     SUBCASE("Test with low nibble AL<9 and flag A unset")
     {
-        flags.unset(Flags::A);
-        flags.set(Flags::C);
+        REQUIRE_CALL(flags_mocked, isSet(Flags::A)).RETURN(false);
+        REQUIRE_CALL(flags_mocked, unset(Flags::A));
+        REQUIRE_CALL(flags_mocked, unset(Flags::C));
+
         registers.AL(0b0110'0001);
         executor.executeNextOperation();
 
-        bool is_any_flag_set = flags.isSet(Flags::A) ||
-                               flags.isSet(Flags::C) ||
-                               flags.isSet(Flags::O) ||
-                               flags.isSet(Flags::D) ||
-                               flags.isSet(Flags::I) ||
-                               flags.isSet(Flags::T) ||
-                               flags.isSet(Flags::S) ||
-                               flags.isSet(Flags::Z) ||
-                               flags.isSet(Flags::P);
-
         REQUIRE(registers.AX() == 0x1);
-        REQUIRE_FALSE(is_any_flag_set);
     };
 
     SUBCASE("Test with low nibble AL<9 and flag A set")
     {
-        flags.set(Flags::A);
-        flags.unset(Flags::C);
+        REQUIRE_CALL(flags_mocked, isSet(Flags::A)).RETURN(true);
+        REQUIRE_CALL(flags_mocked, set(Flags::A));
+        REQUIRE_CALL(flags_mocked, set(Flags::C));
+
         registers.AL(0b0110'0001);
         executor.executeNextOperation();
 
-        bool are_CA_set = flags.isSet(Flags::A) && flags.isSet(Flags::C);
-        bool are_any_other_set = flags.isSet(Flags::O) ||
-                                 flags.isSet(Flags::D) ||
-                                 flags.isSet(Flags::I) ||
-                                 flags.isSet(Flags::T) ||
-                                 flags.isSet(Flags::S) ||
-                                 flags.isSet(Flags::Z) ||
-                                 flags.isSet(Flags::P);
-
         REQUIRE(registers.AX() == 0x0107);
-        REQUIRE(are_CA_set);
-        REQUIRE_FALSE(are_any_other_set);
     };
 }
