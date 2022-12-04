11c850b52546cfb5b48271881956a52eebfd43a7
lucagalbu <luca.galbusera@unibas.ch>
2022-12-04 22:08:46 +0100

    Fixed bug in AAA

--- executor/executor.cpp
+++ executor/executor.cpp
@@ -6,16 +6,16 @@ void Executor::AAA()
 {
     uint8_t lowAL = registers.AL() & 0b0000'1111;
 
-    if (lowAL <= 0x9 && !flags.isSet(Flags::A))
-    {
-        flags.unset(Flags::A);
-        flags.unset(Flags::C);
-    }
-    else
+    if (lowAL > 0x9 || flags.isSet(Flags::A))
     {
         registers.AX(registers.AX() + 0x0106);
         flags.set(Flags::A);
         flags.set(Flags::C);
+    }
+    else
+    {
+        flags.unset(Flags::A);
+        flags.unset(Flags::C);
     }
     registers.AL(registers.AL() & uint8_t(0b0000'1111));
 }
