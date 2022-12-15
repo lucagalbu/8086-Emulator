3938b01dfba37e219a7ce5aa19f78e91d2135dad
lucagalbu <luca.galbusera@unibas.ch>
2022-12-15 18:28:58 +0100

    Added ADC between reg/mem and data

--- executor/executor.cpp
+++ executor/executor.cpp
@@ -67,7 +67,7 @@ void Executor::AAS()
     flags.isSet(Flags::A) ? flags.set(Flags::C) : flags.unset(Flags::C);
 }
 
-void Executor::ADC(uint8_t w, uint8_t param1, uint8_t param2 = 0)
+void Executor::ADC_ac_data(uint8_t w, uint8_t param1, uint8_t param2 = 0)
 {
     uint8_t carry = flags.isSet(Flags::C) ? 1 : 0;
 
@@ -84,5 +84,141 @@ void Executor::ADC(uint8_t w, uint8_t param1, uint8_t param2 = 0)
     {
         cerr << "Operation ADC only recognized is w bit is 0 or 1" << endl;
         exit(1);
+    }
+}
+
+void Executor::ADC_memreg_data()
+{
+    uint8_t opCode = memory[registers.IP()];
+    registers.IP(registers.IP() + 1);
+
+    uint8_t modRM = memory[registers.IP()];
+    registers.IP(registers.IP() + 1);
+
+    uint8_t w = opCode & 0b1;
+    uint8_t s = (opCode >> 1) & 0b1;
+
+    uint8_t mod = (modRM >> 6) & 0b011;
+    uint8_t reg = (modRM >> 3) & 0b111;
+    uint8_t rm = modRM & 0b011;
+
+    uint8_t carry = flags.isSet(Flags::C) ? 1 : 0;
+
+    if (reg != 0b010)
+    {
+        cerr << "Operation ADC between data and mem/reg requires modRM xx010xxx" << endl;
+        exit(1);
+    }
+
+    if (w == 0)
+    {
+        if (mod == 0b00)
+        {
+            uint32_t memoryAddress = getMemAddress(rm, 0);
+            uint8_t operand = codeSegment[registers.IP()];
+            registers.IP(registers.IP() + 1);
+            memory[memoryAddress] = memory[memoryAddress] + operand + carry;
+        }
+        if (mod == 0b01)
+        {
+            uint8_t displacement = codeSegment[registers.IP()];
+            registers.IP(registers.IP() + 1);
+            uint32_t memoryAddress = getMemAddress(rm, displacement);
+            uint8_t operand = codeSegment[registers.IP()];
+            registers.IP(registers.IP() + 1);
+            memory[memoryAddress] = memory[memoryAddress] + operand + carry;
+        }
+        else if (mod == 0b10)
+        {
+            uint16_t displacement = (codeSegment[registers.IP() + 1] << 8) + codeSegment[registers.IP()];
+            registers.IP(registers.IP() + 2);
+            uint32_t memoryAddress = getMemAddress(rm, displacement);
+            uint8_t operand = codeSegment[registers.IP()];
+            registers.IP(registers.IP() + 1);
+            memory[memoryAddress] = memory[memoryAddress] + operand + carry;
+        }
+        else if (mod == 0b11)
+        {
+            uint8_t reg_value = getReg8(rm);
+            uint8_t operand = codeSegment[registers.IP()];
+            registers.IP(registers.IP() + 1);
+            setReg8(rm, reg_value + operand + carry);
+        }
+    }
+    else if (w == 1)
+    {
+        if (mod == 0b00)
+        {
+            uint32_t memoryAddress = getMemAddress(rm, 0);
+            uint16_t operand;
+            if (s == 0)
+            {
+                operand = (codeSegment[registers.IP() + 1] << 8) + codeSegment[registers.IP()];
+                registers.IP(registers.IP() + 2);
+            }
+            else
+            {
+                operand = (((codeSegment[registers.IP()] & 0b1000'0000) * 0b1111'11111) << 8) + codeSegment[registers.IP()];
+                registers.IP(registers.IP() + 1);
+            }
+
+            uint16_t memoryVal = (memory[memoryAddress + 1] << 8) + memory[memoryAddress];
+            uint16_t result = memoryVal + operand + carry;
+
+            memory[memoryAddress] = result & 0b0000'0000'1111'1111;
+            memory[memoryAddress + 1] = result >> 8;
+        }
+        if (mod == 0b01)
+        {
+            uint8_t displacement = codeSegment[registers.IP()];
+            registers.IP(registers.IP() + 1);
+            uint32_t memoryAddress = getMemAddress(rm, displacement);
+            uint16_t operand;
+            if (s == 0)
+            {
+                operand = (codeSegment[registers.IP() + 1] << 8) + codeSegment[registers.IP()];
+                registers.IP(registers.IP() + 2);
+            }
+            else
+            {
+                operand = (((codeSegment[registers.IP()] & 0b1000'0000) * 0b1111'11111) << 8) + codeSegment[registers.IP()];
+                registers.IP(registers.IP() + 1);
+            }
+            memory[memoryAddress] = memory[memoryAddress] + operand + carry;
+        }
+        else if (mod == 0b10)
+        {
+            uint16_t displacement = (codeSegment[registers.IP() + 1] << 8) + codeSegment[registers.IP()];
+            registers.IP(registers.IP() + 2);
+            uint32_t memoryAddress = getMemAddress(rm, displacement);
+            uint16_t operand;
+            if (s == 0)
+            {
+                operand = (codeSegment[registers.IP() + 1] << 8) + codeSegment[registers.IP()];
+                registers.IP(registers.IP() + 2);
+            }
+            else
+            {
+                operand = (((codeSegment[registers.IP()] & 0b1000'0000) * 0b1111'11111) << 8) + codeSegment[registers.IP()];
+                registers.IP(registers.IP() + 1);
+            }
+            memory[memoryAddress] = memory[memoryAddress] + operand + carry;
+        }
+        else if (mod == 0b11)
+        {
+            uint16_t reg_value = getReg16(rm);
+            uint16_t operand;
+            if (s == 0)
+            {
+                operand = (codeSegment[registers.IP() + 1] << 8) + codeSegment[registers.IP()];
+                registers.IP(registers.IP() + 2);
+            }
+            else
+            {
+                operand = (((codeSegment[registers.IP()] & 0b1000'0000) * 0b1111'11111) << 8) + codeSegment[registers.IP()];
+                registers.IP(registers.IP() + 1);
+            }
+            setReg16(rm, reg_value + operand + carry);
+        }
     }
 }
--- executor/executor.hpp
+++ executor/executor.hpp
@@ -13,20 +13,193 @@ private:
     Registers &registers;
     Flags &flags;
 
-    const uint8_t *memory;
-    const uint8_t *codeSegment;
-    const uint8_t *dataSegment;
-    const uint8_t *stackSegment;
-    const uint8_t *extraSegment;
+    uint8_t *memory;
+    uint8_t *codeSegment;
+    uint8_t *dataSegment;
+    uint8_t *stackSegment;
+    uint8_t *extraSegment;
+
+    uint32_t getMemAddress(uint8_t rm, uint16_t displacement = 0)
+    {
+        switch (rm)
+        {
+        case 0b000:
+            return (registers.DS() * 0x10) + registers.BX() + registers.SI() + displacement;
+            break;
+        case 0b001:
+            return (registers.DS() * 0x10) + registers.BX() + registers.DI() + displacement;
+            break;
+        case 0b010:
+            return (registers.SS() * 0x10) + registers.BP() + registers.SI() + displacement;
+            break;
+        case 0b011:
+            return (registers.SS() * 0x10) + registers.BP() + registers.DI() + displacement;
+            break;
+        case 0b100:
+            return (registers.DS() * 0x10) + registers.SI() + displacement;
+            break;
+        case 0b101:
+            return (registers.DS() * 0x10) + registers.DI() + displacement;
+            break;
+        case 0b110:
+            return (registers.SS() * 0x10) + registers.BP() + displacement;
+            break;
+        case 0b111:
+            return (registers.DS() * 0x10) + registers.BX() + registers.SI() + displacement;
+            break;
+        default:
+            cerr << hex;
+            cerr << "rm=" << (unsigned int)rm << " of modRM not recognized." << endl;
+            cerr << dec;
+            exit(1);
+        }
+    }
+
+    uint8_t getReg8(uint8_t code)
+    {
+        switch (code)
+        {
+        case 0x0:
+            return (registers.AL());
+            break;
+        case 0x1:
+            return (registers.CL());
+            break;
+        case 0x2:
+            return (registers.DL());
+            break;
+        case 0x3:
+            return (registers.BL());
+            break;
+        case 0x4:
+            return (registers.AH());
+            break;
+        case 0x5:
+            return (registers.CH());
+            break;
+        case 0x6:
+            return (registers.DH());
+            break;
+        case 0x7:
+            return (registers.BH());
+            break;
+        default:
+            cerr << "reg code not recognized" << endl;
+            exit(1);
+        }
+    }
+
+    void setReg8(uint8_t code, uint8_t value)
+    {
+        switch (code)
+        {
+        case 0x0:
+            registers.AL(value);
+            break;
+        case 0x1:
+            registers.CL(value);
+            break;
+        case 0x2:
+            registers.DL(value);
+            break;
+        case 0x3:
+            registers.BL(value);
+            break;
+        case 0x4:
+            registers.AH(value);
+            break;
+        case 0x5:
+            registers.CH(value);
+            break;
+        case 0x6:
+            registers.DH(value);
+            break;
+        case 0x7:
+            registers.BH(value);
+            break;
+        default:
+            cerr << "reg code not recognized" << endl;
+            exit(1);
+        }
+    }
+
+    uint16_t getReg16(uint8_t code)
+    {
+        switch (code)
+        {
+        case 0x0:
+            return (registers.AX());
+            break;
+        case 0x1:
+            return (registers.CX());
+            break;
+        case 0x2:
+            return (registers.DX());
+            break;
+        case 0x3:
+            return (registers.BX());
+            break;
+        case 0x4:
+            return (registers.SP());
+            break;
+        case 0x5:
+            return (registers.BP());
+            break;
+        case 0x6:
+            return (registers.SI());
+            break;
+        case 0x7:
+            return (registers.DI());
+            break;
+        default:
+            cerr << "reg code not recognized" << endl;
+            exit(1);
+        }
+    }
+
+    void setReg16(uint8_t code, uint16_t value)
+    {
+        switch (code)
+        {
+        case 0x0:
+            registers.AX(value);
+            break;
+        case 0x1:
+            registers.CX(value);
+            break;
+        case 0x2:
+            registers.DX(value);
+            break;
+        case 0x3:
+            registers.BX(value);
+            break;
+        case 0x4:
+            registers.SP(value);
+            break;
+        case 0x5:
+            registers.BP(value);
+            break;
+        case 0x6:
+            registers.SI(value);
+            break;
+        case 0x7:
+            registers.DI(value);
+            break;
+        default:
+            cerr << "reg code not recognized" << endl;
+            exit(1);
+        }
+    }
 
     void AAA();
     void AAD(uint8_t param1);
     void AAM(uint8_t param1);
     void AAS();
-    void ADC(uint8_t w, uint8_t param1, uint8_t param2);
+    void ADC_ac_data(uint8_t w, uint8_t param1, uint8_t param2);
+    void ADC_memreg_data();
 
 public:
-    Executor(Registers &registers, Flags &flags, const uint8_t *memory, const size_t segment_size_byte) : registers(registers), flags(flags), memory(memory)
+    Executor(Registers &registers, Flags &flags, uint8_t *memory, const size_t segment_size_byte) : registers(registers), flags(flags), memory(memory)
     {
         codeSegment = &memory[registers.CS() * segment_size_byte];
         dataSegment = &memory[registers.DS() * segment_size_byte];
@@ -45,11 +218,11 @@ public:
             registers.IP(registers.IP() + 1);
             break;
         case 0xD5:
-            AAD(memory[++codeOffset]);
+            AAD(codeSegment[++codeOffset]);
             registers.IP(registers.IP() + 2);
             break;
         case 0xD4:
-            AAM(memory[++codeOffset]);
+            AAM(codeSegment[++codeOffset]);
             registers.IP(registers.IP() + 2);
             break;
         case 0x3F:
@@ -57,12 +230,15 @@ public:
             registers.IP(registers.IP() + 1);
             break;
         case 0x14:
-            ADC(0, memory[++codeOffset], 0);
+            ADC_ac_data(0, codeSegment[++codeOffset], 0);
             registers.IP(registers.IP() + 2);
             break;
         case 0x15:
-            ADC(1, memory[codeOffset + 1], memory[codeOffset + 2]);
+            ADC_ac_data(1, codeSegment[codeOffset + 1], codeSegment[codeOffset + 2]);
             registers.IP(registers.IP() + 3);
+            break;
+        case 0x81:
+            ADC_memreg_data();
             break;
         default:
             cout << "OpCode 0x" << hex << (unsigned int)opCode << dec << " not recognized" << endl;
