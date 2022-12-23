f80d58870e7329fcf3a23bf0e39aa31c7b1f29c9
lucagalbu <luca.galbusera@unibas.ch>
2022-12-23 15:26:40 +0100

    Unit tests: using mocked Memory

--- tests/executor_test.cpp
+++ tests/executor_test.cpp
@@ -17,15 +17,23 @@ public:
     MAKE_MOCK1(unset, void(FlagMasks flag));
 };
 
+class Memory_mocked : public Memory
+{
+public:
+    MAKE_MOCK1(readByte, uint8_t(MemoryAddress));
+    MAKE_MOCK2(setByte, void(MemoryAddress, uint8_t));
+    MAKE_MOCK1(readWord, uint16_t(MemoryAddress));
+    MAKE_MOCK2(setWord, void(MemoryAddress, uint16_t));
+};
+
 TEST_CASE("Test AAA")
 {
     Registers registers;
     Flags_mocked flags_mocked;
-    Memory memory;
+    Memory_mocked memory_mocked;
 
-    Executor executor(registers, flags_mocked, memory);
+    Executor executor(registers, flags_mocked, memory_mocked);
 
-    memory.setByte(MemoryAddress(0, 0), 0x37);
     registers.AX(0);
 
     SUBCASE("Test with low nibble AL>9 and flag A unset")
@@ -33,6 +41,7 @@ TEST_CASE("Test AAA")
         ALLOW_CALL(flags_mocked, isSet(Flags::A)).RETURN(false);
         REQUIRE_CALL(flags_mocked, set(Flags::A));
         REQUIRE_CALL(flags_mocked, set(Flags::C));
+        ALLOW_CALL(memory_mocked, readByte(trompeloeil::_)).RETURN(0x37);
 
         registers.AL(0b0000'1111);
         executor.executeNextOperation();
@@ -45,6 +54,7 @@ TEST_CASE("Test AAA")
         REQUIRE_CALL(flags_mocked, isSet(Flags::A)).RETURN(false);
         REQUIRE_CALL(flags_mocked, unset(Flags::A));
         REQUIRE_CALL(flags_mocked, unset(Flags::C));
+        ALLOW_CALL(memory_mocked, readByte(trompeloeil::_)).RETURN(0x37);
 
         registers.AL(0b0110'0001);
         executor.executeNextOperation();
@@ -57,6 +67,7 @@ TEST_CASE("Test AAA")
         REQUIRE_CALL(flags_mocked, isSet(Flags::A)).RETURN(true);
         REQUIRE_CALL(flags_mocked, set(Flags::A));
         REQUIRE_CALL(flags_mocked, set(Flags::C));
+        ALLOW_CALL(memory_mocked, readByte(trompeloeil::_)).RETURN(0x37);
 
         registers.AL(0b0110'0001);
         executor.executeNextOperation();
